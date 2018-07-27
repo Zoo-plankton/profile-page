704a42f5d269060bdcafa07db77d8626a95dc61d
zooplankton <33819748+zooplankton@users.noreply.github.com>
2018-07-27 00:11:41 -0400

    fix body gradient and text inconsistencies

--- components/projects.js
+++ components/projects.js
@@ -8,13 +8,13 @@ var projects = [
   {
     img: "../img/websiteThumb.jpg",
     title: "Site Design with Assets",
-    description: "A mock site designed in Photoshop with animated assets created on CodePen using CSS and JavaScript",
+    description: "A mock site designed in Photoshop with animated assets created on CodePen using CSS & JavaScript",
     link: "/race"
   },
   {
     img: "../img/yelpcamp.jpg",
     title: "Camping Website",
-    description: "RESTful app made with Bootstrap Node.js, Express, & MongoDB. Database hosted on mLab and app deployed on Heroku.",
+    description: "RESTful app made with Bootstrap Node.js, Express, & MongoDB. Database hosted on mLab & app deployed on Heroku.",
     link: "https://protected-scrubland-66811.herokuapp.com"
   }
 ]
